a86757147a67ab7723db6f953064acb116001d20
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-08-25 16:39:15 +0900

    投稿画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,23 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
   end
 
   def new
+  end
+
+  def create
+    Post.create(image: "", text: "")
+  end
+
+  def show
+    @posts = Post.order("created_at DESC")
+  end
+
+  private
+
+  def post_params
+    params.permit(:name, :image, :text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  get  'posts/new'  =>  'posts#new'
+  get  'posts/new' => 'posts#new'
+  get  'posts/show'=> 'posts#show'
+  post 'posts'     => 'posts#create'
 end
