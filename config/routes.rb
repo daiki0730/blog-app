918111570737d2d420c3455ee6d1bb2c38ffd447
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-08-28 19:49:20 +0900

    削除ボタンを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
-  get  'posts/new' => 'posts#new'
-  get  'posts/show'=> 'posts#show'
-  post 'posts'     => 'posts#create'
-  get  'users/:id' =>  'users#show'
+  root    'posts#index'
+  get     'posts/new'   =>  'posts#new'
+  get     'posts/show'  =>  'posts#show'
+  post    'posts'       =>  'posts#create'
+  get     'users/:id'   =>  'users#show'
+  delete  'posts/:id'   =>  'posts#destroy'
 end
