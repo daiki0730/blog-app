02222ac94ec2fb1fb8e79eba9b06e5d5e3a91a5d
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-09-16 16:21:21 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,4 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
-  private
-
-  def geocode
-    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key= ENV["GOOGLE_MAP_KEY"] ")
-    res = HTTP.get(uri).to_s
-    response = JSON.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
-  end
-
 end
