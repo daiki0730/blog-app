a7a399de97cbe9b486707d84336edccdefb94cbc
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-09-18 20:14:28 +0900

    イメージ画像を変更
    アプリのイメージに合うように変更。

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,10 @@ set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  TWITTER_KEY: ENV["TWITTER_KEY"],
+  TWITTER_SECRET_KEY: ENV["TWITTER_SECRET_KEY"],
+  GOOGLE_MAP_KEY: ENV["GOOGLE_MAP_KEY"]
 }
 
 # secrets.yml用のシンボリックリンクを追加
